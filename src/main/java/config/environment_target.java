eb2ed274f893b24a892765097649260c41eb4834
rahmamawaddahf <111490618+rahmamawaddahf@users.noreply.github.com>
2023-10-24 02:16:08 +0700

    HW6-SQA

---
+++ src/main/java/config/environment_target.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class environment_target {
+    protected WebDriver driver;
+    protected String BaseUrl = "https://www.saucedemo.com/";
+    protected String ProductListPageUrl = "https://www.saucedemo.com/inventory.html";
+}
